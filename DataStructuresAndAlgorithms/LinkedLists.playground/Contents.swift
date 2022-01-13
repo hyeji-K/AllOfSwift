fc16566b9bd4ee132619b66deadffa6dfcf8594f
Khyeji <khjji0502@gmail.com>
2022-01-13 21:15:36 +0900

    linkedList: adding vaiues to the list

--- DataStructuresAndAlgorithms/LinkedLists.playground/Contents.swift
+++ DataStructuresAndAlgorithms/LinkedLists.playground/Contents.swift
@@ -1,3 +1,45 @@
-import UIKit
 
-var greeting = "Hello, playground"
+// Linked Lists
+/* some theoretical advantages
+ - Constant time insertion and removal from the front of the list.
+ - Reliable performance characteristics.
+ */
+
+// "creating and linking nodes"
+let node1 = Node(value: 1)
+let node2 = Node(value: 2)
+let node3 = Node(value: 3)
+
+node1.next = node2
+node2.next = node3
+
+print(node1)
+
+// push
+var list = LinkedList<Int>()
+list.push(3)
+list.push(2)
+list.push(1)
+
+print(list)
+
+// append
+var appendList = LinkedList<Int>()
+appendList.append(1)
+appendList.append(2)
+appendList.append(3)
+
+print(appendList)
+
+// inserting at a particular index
+var insertList = LinkedList<Int>()
+insertList.push(3)
+insertList.push(2)
+insertList.push(1)
+
+print("Before inserting: \(insertList)")
+var middleNode = insertList.node(at: 1)!
+for _ in 1...4 {
+    middleNode = insertList.insert(-1, after: middleNode)
+}
+print("After inserting: \(insertList)")
---
+++ DataStructuresAndAlgorithms/LinkedLists.playground/Sources/LinkedList.swift
@@ -0,0 +1,68 @@
+import Foundation
+
+public struct LinkedList<Value> {
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+    
+    public init() { }
+    
+    public var isEmpty: Bool {
+        head == nil
+    }
+    
+    public mutating func push(_ value: Value) {
+        head = Node(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+    
+    public mutating func append(_ value: Value) {
+        
+        // 1
+        guard !isEmpty else {
+            push(value)
+            return
+        }
+        
+        //2
+        tail!.next = Node(value: value)
+        
+        //3
+        tail = tail!.next
+    }
+    
+    public func node(at index: Int) -> Node<Value>? {
+        //1
+        var currentNode = head
+        var currentIndex = 0
+        
+        //2
+        while currentNode != nil && currentIndex < index {
+            currentNode = currentNode!.next
+            currentIndex += 1
+        }
+        
+        return currentNode
+    }
+    
+    @discardableResult
+    public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+        guard tail !== node else {
+            append(value)
+            return tail!
+        }
+        
+        node.next = Node(value: value, next: node.next)
+        return node.next!
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        guard let head = head else {
+            return "Empty list"
+        }
+        return String(describing: head)
+    }
+}
