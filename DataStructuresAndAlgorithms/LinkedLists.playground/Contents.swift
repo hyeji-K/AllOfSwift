8740024d55039899b7e324c179011146cdb6c6ed
Khyeji <khjji0502@gmail.com>
2022-01-14 18:57:20 +0900

    LinkedList: adding removing values from the list

--- DataStructuresAndAlgorithms/LinkedLists.playground/Contents.swift
+++ DataStructuresAndAlgorithms/LinkedLists.playground/Contents.swift
@@ -43,3 +43,40 @@ for _ in 1...4 {
     middleNode = insertList.insert(-1, after: middleNode)
 }
 print("After inserting: \(insertList)")
+
+// pop
+var popTestList = LinkedList<Int>()
+popTestList.push(3)
+popTestList.push(2)
+popTestList.push(1)
+
+print("Before popping list: \(popTestList)")
+let poppedValue = popTestList.pop()
+print("After popping list: \(popTestList)")
+print("Popped value: " + String(describing: poppedValue))
+
+// removing the last node
+var lastRemoveList = LinkedList<Int>()
+lastRemoveList.push(3)
+lastRemoveList.push(2)
+lastRemoveList.push(1)
+
+print("Before removing last node: \(lastRemoveList)")
+let removedValue = lastRemoveList.removeLast()
+
+print("After removing last node: \(lastRemoveList)")
+print("Removed value: " + String(describing: removedValue))
+
+// removing a node after a particular node
+var particularRemoveList = LinkedList<Int>()
+particularRemoveList.push(3)
+particularRemoveList.push(2)
+particularRemoveList.push(1)
+
+print("Before removing at particular index: \(particularRemoveList)")
+let index = 1
+let node = particularRemoveList.node(at: index - 1)!
+let ParicularRemovedValue = particularRemoveList.remove(after: node)
+
+print("After removing at index \(index): \(particularRemoveList)")
+print("Removed value: " + String(describing: ParicularRemovedValue))
--- DataStructuresAndAlgorithms/LinkedLists.playground/Sources/LinkedList.swift
+++ DataStructuresAndAlgorithms/LinkedLists.playground/Sources/LinkedList.swift
@@ -56,6 +56,51 @@ public struct LinkedList<Value> {
         node.next = Node(value: value, next: node.next)
         return node.next!
     }
+    
+    @discardableResult
+    public mutating func pop() -> Value? {
+        defer {
+            head = head?.next
+            if isEmpty {
+                tail = nil
+            }
+        }
+        return head?.value
+    }
+    
+    @discardableResult
+    public mutating func removeLast() -> Value? {
+        guard let head = head else {
+            return nil
+        }
+        
+        guard head.next != nil else {
+            return pop()
+        }
+        
+        var prev = head
+        var current = head
+        
+        while let next = current.next {
+            prev = current
+            current = next
+        }
+        
+        prev.next = nil
+        tail = prev
+        return current.value
+    }
+    
+    @discardableResult
+    public mutating func remove(after node: Node<Value>) -> Value? {
+        defer {
+            if node.next === tail {
+                tail = node
+            }
+            node.next = node.next?.next
+        }
+        return node.next?.value
+    }
 }
 
 extension LinkedList: CustomStringConvertible {
