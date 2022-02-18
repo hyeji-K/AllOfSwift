e155f468610e24ff321a42021a8b192376678ab0
Khyeji <khjji0502@gmail.com>
2022-02-18 18:36:27 +0900

    UIBezierPath - cat shape

--- ARKit/World Tracking/World Tracking/ViewController.swift
+++ ARKit/World Tracking/World Tracking/ViewController.swift
@@ -22,13 +22,34 @@ class ViewController: UIViewController {
 
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
-        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+//        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+//        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+//        node.geometry = SCNCone(topRadius: 0.3, bottomRadius: 0.3, height: 0.3)
+//        node.geometry = SCNCylinder(radius: 0.2, height: 0.2)
+//        node.geometry = SCNSphere(radius: 0.1)
+//        node.geometry = SCNTube(innerRadius: 0.2, outerRadius: 0.3, height: 0.5)
+//        node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1)
+//        node.geometry = SCNPlane(width: 0.2, height: 0.2)
+//        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: -0.2, y: 0.2))
+        path.addLine(to: CGPoint(x: 0, y: 0.4))
+        path.addLine(to: CGPoint(x: 0, y: 0.7))
+        path.addLine(to: CGPoint(x: 0.2, y: 0.4))
+        path.addLine(to: CGPoint(x: 0.4, y: 0.4))
+        path.addLine(to: CGPoint(x: 0.6, y: 0.7))
+        path.addLine(to: CGPoint(x: 0.6, y: 0.4))
+        path.addLine(to: CGPoint(x: 0.8, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.6, y: 0))
+        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+        node.geometry = shape
+        node.geometry?.firstMaterial?.specular.contents = UIColor.orange
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        node.position = SCNVector3(x, y, z)
+//        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+//        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+//        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        node.position = SCNVector3(0, 0, -0.3)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     
