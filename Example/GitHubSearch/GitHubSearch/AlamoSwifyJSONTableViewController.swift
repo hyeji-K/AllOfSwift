de212a0a8d958233fc7b73ebf48ceb22e625f1ea
Khyeji <khjji0502@gmail.com>
2022-02-21 19:20:58 +0900

    githubSearch: SwiftyJSON

---
+++ Example/GitHubSearch/GitHubSearch/AlamoSwifyJSONTableViewController.swift
@@ -0,0 +1,104 @@
+//
+//  AlamoSwifyJSONTableViewController.swift
+//  GitHubSearch
+//
+//  Created by heyji on 2022/02/21.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class AlamoSwifyJSONTableViewController: UITableViewController {
+    let accept = "application/vnd.github.v3+json"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        search(query: "swift")
+    }
+    
+    func search(query: String) {
+        let strURL = "https://api.github.com/search/repositories"
+        let params: Parameters = ["q": query]
+        let headers: HTTPHeaders = ["Accept": accept]
+        let alamo = AF.request(strURL, method: .get, parameters: params, headers: headers)
+        alamo.responseJSON { response in
+            switch response.result {
+            case .success(let data):
+                let json = JSON(data)
+                let name = json["items"].arrayValue[0]["language"]
+                print(name)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
