54bdd64af88d47bf5af8f2eff1a86e3ad616af5e
Khyeji <khjji0502@gmail.com>
2022-04-01 15:34:09 +0900

    No.8393

---
+++ Example/algorithm/algorithm/8393.swift
@@ -0,0 +1,15 @@
+//
+//  8393.swift
+//  algorithm
+//
+//  Created by heyji on 2022/04/01.
+//
+
+// 백준 8393 - 합
+let input = Int(readLine()!)!
+var value: Int = 0
+
+for i in 1...input {
+    value += i
+}
+print(value)
