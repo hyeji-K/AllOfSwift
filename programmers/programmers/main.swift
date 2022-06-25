818bc40f7b59f4c01c3854a038f7b0d1df486089
Khyeji <khjji0502@gmail.com>
2022-06-25 13:09:27 +0900

    시간초과 테스트 케이스 해결

---
+++ programmers/programmers/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  programmers
+//
+//  Created by heyji on 2022/06/24.
+//
+
+import Foundation
--- programmers/programmers/제일작은수제거하기.swift
+++ programmers/programmers/제일작은수제거하기.swift
@@ -9,22 +9,28 @@ import Foundation
 
 func solution(_ arr:[Int]) -> [Int] {
     
-    var result: [Int] = arr
+    var result: [Int] = []
+    let minValue: Int = arr.min()!
+    print(minValue)
     
-    if arr.count > 1 {
-        for i in 0..<arr.count {
-            if result[i] == arr.min()! {
-                result.remove(at: i)
-                break
-            } else {
-                continue
-            }
+    for i in 0..<arr.count {
+        if arr[i] == minValue {
+            continue
+        } else {
+            result.append(arr[i])
         }
-        return result
     }
     
-    return [-1]
+    return result.isEmpty ? [-1] : result
 }
 
 print(solution([4,3,2,1]))
 print(solution([10]))
+print(solution([2,3,8,2,9]))
+
+
+// 오버헤드로 인한 시간초과?
+/*
+ 작은 수가 두 개 이상일때, index 때문에 발생한 시간초과 실패
+ 배열에서 제거한다는 생각 말고 작은 수를 제외한 값들을 새로운 배열에 추가한다고 생각
+ */
