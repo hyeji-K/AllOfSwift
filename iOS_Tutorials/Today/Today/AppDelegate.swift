7c352db342dcf4154246cc2bc0bf8a9b5b5fec31
Khyeji <khjji0502@gmail.com>
2022-10-14 00:25:30 +0900

    Feat: Displaying reminder details

--- iOS_Tutorials/Today/Today/AppDelegate.swift
+++ iOS_Tutorials/Today/Today/AppDelegate.swift
@@ -14,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().tintColor = .todayPrimaryTint // 앱에 있는 모든 탐색 모음의 기본 모양을 변경
+        UINavigationBar.appearance().backgroundColor = .todayNavigationBackground
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground() // 불투명 탐색 모음
+//        navBarAppearance.configureWithTransparentBackground() // 반투명탐색모음
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
         return true
     }
 
